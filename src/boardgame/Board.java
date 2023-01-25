618b96dd692742938935cbb3c4ff7471d1afd6b6
Samm-Rod <Smk191419@gmail.com>
2023-01-25 20:50:26 -0300

    Special move - castling

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -23,9 +23,9 @@ public class Board {
         return pieces[row][column];
     }
 
-    public Piece piece(Position position){
-        if(!positionExists(position)){
-            throw new BoardException("Position not the board");
+    public Piece piece(Position position) {
+        if (!positionExists(position)) {
+            throw new BoardException("Position not on the board");
         }
         return pieces[position.getRow()][position.getColumn()];
     }
